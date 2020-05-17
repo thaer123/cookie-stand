fca74ce546c2019e65f846f46d928ae2a50a388a
thaer <thaerkhaled13@gmail.com>
2020-05-17 21:55:04 +0300

    starting Salmon Cookies shope code

---
+++ js/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// Array for business hours
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// Variable storing array of location objects + variable for table
+var allLocations = [];
+// var locationTable = document.getElementById('locationSales');
+// var storeForm = document.getElementById('store-form');
+
+// Constructor function for stores
+function CookieStore(location, minCustomers, maxCustomers, avgSale) {
+  this.location = location;
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers;
+  this.avgSale = avgSale;
+  this.avgCustomersHour = 0;
+  this.avgCookiesHour = 0;
+  this.customersArray = [];
+  this.cookiesArray = [];
+  this.totalCookies = 0;
+  this.getAvgCustomersHour = function (){
+    for (var i = 0; i < hours.length; i++){
+      this.avgCustomersHour = Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
+      this.customersArray.push(this.avgCustomersHour);
+    }
+  };
+  this.getAvgCookiesHour = function(){
+    for (var i = 0; i < this.customersArray.length; i++){
+      this.avgCookiesHour = Math.ceil(this.customersArray[i] * this.avgSale);
+      this.cookiesArray.push(this.avgCookiesHour);
+    }
+  };
+  this.getTotalSold = function(){
+    this.totalCookies = this.cookiesArray.reduce(function (a,b) {
+      return a + b;
+    }, 0);
+  };
+
+  this.getAvgCustomersHour();
+  this.getAvgCookiesHour();
+  this.getTotalSold();
+  allLocations.push(this);
+}
+
